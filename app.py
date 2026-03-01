358fab39c195e63c81bd6367b63ece3d4d1ed2e8
Saurabh-Prajapati123 <saurabhprajapati246@gmail.com>
2026-03-01 09:50:46 +0530

    heart disease railway deploy

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, app, render_template, request
+import numpy as np
+import joblib
+app = Flask(__name__)
+
+model = joblib.load("heart_disease_model.pkl")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_features = np.array(features).reshape(1, -1)
+    prediction = model.predict(final_features)
+
+    result = "Heart Disease Detected 💔" if prediction[0] == 1 else "No Heart Disease ❤️"
+
+    return render_template("index.html", prediction_text=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
