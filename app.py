1f5c177eed03167d63556f7efc3f36d5c1a7822b
Saurabh-Prajapati123 <saurabhprajapati246@gmail.com>
2026-03-01 10:10:24 +0530

    railway port fix

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, app, render_template, request
 import numpy as np
 import joblib
 app = Flask(__name__)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 model = joblib.load("heart_disease_model.pkl")
 
